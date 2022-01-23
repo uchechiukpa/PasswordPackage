4688a9c9c00fbabdb18920d48fa24c60cf83e8e6
uchechi <uchechi.ukpa@globalaccelerex.com>
2022-01-23 22:25:24 +0100

    compiled index.ts, updated package version

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
-export {};
+export * from "./passwordchecker";
+export * from "./passwordgenerator";
+export * from "./passwordhasher";
 //# sourceMappingURL=index.js.map
--- dist/tests/passwordhasher.test.js
+++ dist/tests/passwordhasher.test.js
@@ -2,7 +2,6 @@ import { comparePassword, passwordHasher } from "../passwordhasher";
 test("hashing password meets constraints", function () {
     var password = "m8y09*Gik099Pt/";
     var hashedPassword = passwordHasher(password);
-    console.log(hashedPassword);
     var hash = "d4b77bd79123c3cded49004b77ffae9aaca4d585f4a91c35d45d6ffdc0adb858e1c15a44aae681a2d2c1d98be670f0ef7133d5b0aea432a7befc61bf68e46544";
     expect(hashedPassword.length).toBe(128);
     var comparedPassword = comparePassword(hash, password);
